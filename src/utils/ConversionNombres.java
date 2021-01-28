35d13b56e2f130f2cd07f423502af4f48e8f82b3
Alexis <alexis.goupillot.dev@gmail.com>
2021-01-28 10:28:20 +0100

    TP4 Finished

---
+++ src/utils/ConversionNombres.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class ConversionNombres {
+
+	public static void main(String[] args) {
+
+		String chaine = "12";
+		int chaineInt = Integer.parseInt(chaine);
+		
+		int int1 = 15;
+		int int2 = 6;
+		
+		System.out.println(Integer.max(int1, int2)); 
+		
+		
+
+	}
+
+}
